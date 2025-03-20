7634e49ac3a341a616d324dc0ac7e06f967ab301
Julio César Gama <me@juliocesargama.tech>
2025-03-19 23:30:17 -0300

    initial structure project

---
+++ src/main/kotlin/org/univesp/natalagapebackend/NatalAgapeBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.univesp.natalagapebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NatalAgapeBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<NatalAgapeBackendApplication>(*args)
+}
---
+++ src/test/kotlin/org/univesp/natalagapebackend/NatalAgapeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.univesp.natalagapebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NatalAgapeBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
