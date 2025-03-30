06d2164a8761374d7504b4705194d976b61f183b
Julio César Gama <me@juliocesargama.tech>
2025-03-30 19:55:39 -0300

    minor fix

--- src/main/kotlin/org/univesp/natalagapebackend/repositories/SponsorRepository.kt
+++ src/main/kotlin/org/univesp/natalagapebackend/repositories/SponsorRepository.kt
@@ -1,7 +1,9 @@
 package org.univesp.natalagapebackend.repositories
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import org.univesp.natalagapebackend.models.Sponsor
 
+@Repository
 interface SponsorRepository : JpaRepository<Sponsor, Long> {
 }
